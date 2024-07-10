ad36bc4b92996af030ace1e331b7b645af72547e
Vesperon <sinitchikoduo2@gmail.com>
2024-07-11 00:31:36 +0800

    2nd Commit

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,5 +1,6 @@
 import './Chessboard.css';
 import Tile from '../Tiles/Tile';
+import React, { useRef } from 'react';
 const verticalAxis = ["1","2","3","4","5","6","7","8"];
 const horizontalAxis =["a","b","c","d","e","f","g","h"];
 
@@ -17,10 +18,16 @@ for(let p=0; p < 8; p++){
 
     pieces.push({ image: `assets/images/rook_${type}.png`, x: 0, y});
     pieces.push({ image: `assets/images/rook_${type}.png`, x: 0, y});
+    pieces.push({ image: `assets/images/rook_${type}.png`, x: 7, y});
+    pieces.push({ image: `assets/images/rook_${type}.png`, x: 7, y});
     pieces.push({ image: `assets/images/knight_${type}.png`, x: 1, y});
     pieces.push({ image: `assets/images/knight_${type}.png`, x: 1, y});
+    pieces.push({ image: `assets/images/knight_${type}.png`, x: 6, y});
+    pieces.push({ image: `assets/images/knight_${type}.png`, x: 6, y});
     pieces.push({ image: `assets/images/bishop_${type}.png`, x: 2, y});
     pieces.push({ image: `assets/images/bishop_${type}.png`, x: 2, y});
+    pieces.push({ image: `assets/images/bishop_${type}.png`, x: 5, y});
+    pieces.push({ image: `assets/images/bishop_${type}.png`, x: 5, y});
     pieces.push({ image: `assets/images/queen_${type}.png`, x: 3, y});
     pieces.push({ image: `assets/images/king_${type}.png`, x: 4, y});
 }
@@ -35,32 +42,74 @@ for(let i = 0; i < 8 ; i++){
 
 
 
-pieces.push({ image: "assets/images/rook_b.png", x: 0, y:7});
-pieces.push({ image: "assets/images/rook_b.png", x: 7, y:7});
-pieces.push({ image: "assets/images/rook_w.png", x: 0, y:0});
-pieces.push({ image: "assets/images/rook_w.png", x: 7, y:0});
-
-
-pieces.push({ image: "assets/images/knight_b.png", x: 1, y:7});
-pieces.push({ image: "assets/images/knight_b.png", x: 6, y:7});
-pieces.push({ image: "assets/images/knight_w.png", x: 1, y:0});
-pieces.push({ image: "assets/images/knight_w.png", x: 6, y:0});
-
-pieces.push({ image: "assets/images/bishop_b.png", x: 2, y:7});
-pieces.push({ image: "assets/images/bishop_b.png", x: 5, y:7});
-pieces.push({ image: "assets/images/bishop_w.png", x: 2, y:0});
-pieces.push({ image: "assets/images/bishop_w.png", x: 5, y:0});
-
-pieces.push({ image: "assets/images/queen_b.png", x: 3, y:7});
-pieces.push({ image: "assets/images/king_b.png", x: 4, y:7});
-pieces.push({ image: "assets/images/queen_w.png", x: 3, y:0});
-pieces.push({ image: "assets/images/king_w.png", x: 4, y:0});
-
-
 
 
 
 export default function Chessboard(){
+    const chessboardRef = useRef<HTMLDivElement>(null);
+
+    let activePiece: HTMLElement | null = null;
+
+    function grapPiece(e: React.MouseEvent){
+        const element = e.target as HTMLElement;
+        if(element.classList.contains("chess-piece")){
+            const x = e.clientX - 30;
+            const y = e.clientY - 33;
+            element.style.position = "absolute";
+            element.style.left = `${x}px`;
+            element.style.top = `${y}px`;
+    
+            activePiece = element;
+        }
+    }
+    
+    function movePiece(e: React.MouseEvent){
+        const chessboard = chessboardRef.current;
+        if(activePiece && chessboard){
+            const minX = chessboard.offsetLeft - 20;
+            const minY = chessboard.offsetTop - 15;
+            const maxX = chessboard.offsetLeft + chessboard.clientWidth - 45;
+            const maxY = chessboard.offsetTop + chessboard.clientHeight - 48;
+            const x = e.clientX - 30;
+            const y = e.clientY - 33;
+            activePiece.style.position = "absolute";
+            
+            if(x < minX){
+                activePiece.style.left = `${minX}px`;
+            }
+
+            else if(x > maxX){
+                activePiece.style.left = `${maxX}px`;
+            }
+
+            else{
+                activePiece.style.left = `${x}px`;
+            }
+
+            if(y < minY){
+                activePiece.style.top = `${minY}px`;
+            }
+
+            else if(y > maxY){
+                activePiece.style.top = `${maxY}px`;
+            }
+
+            else{
+                activePiece.style.top = `${y}px`;
+            }
+
+
+
+        }
+        
+    }
+    
+    function dropPiece(e: React.MouseEvent){
+        if(activePiece){
+            activePiece = null;
+        }
+    }
+
     let board = [];
 
     for(let j= verticalAxis.length - 1; j>=0; j-- ){
@@ -74,9 +123,15 @@ export default function Chessboard(){
                 }
             });
 
-            board.push(<Tile image={image} number={number} />);
+            board.push(<Tile key={`${j},${i}`} image={image} number={number} />);
         }
     }
 
-    return <div id="chessboard">{board}</div>
+    return <div 
+    onMouseMove={e => movePiece(e)} 
+    onMouseDown={e => grapPiece(e)} 
+    onMouseUp={e => dropPiece(e)}
+    id="chessboard"
+    ref={chessboardRef}
+    >{board}</div>
 }
